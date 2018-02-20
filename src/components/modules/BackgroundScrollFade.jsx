a0098c756c054da39f39b6b1331f956c2a3930cf
thomasheimstad <thomasheimstad@gmail.com>
2018-02-20 19:09:11 +0100

    test IE check

--- src/components/modules/BackgroundScrollFade.jsx
+++ src/components/modules/BackgroundScrollFade.jsx
@@ -4,6 +4,21 @@ export default class BackgroundScrollFade extends React.Component {
   state = {
     mediaWidth: 0
   }
+  GetIEVersion = () => {
+    let sAgent = window.navigator.userAgent;
+    let Idx = sAgent.indexOf("MSIE");
+
+    // If IE, return version number.
+    if (Idx > 0)
+      return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+    // If IE 11 then look for Updated user agent string.
+    else if (!!navigator.userAgent.match(/Trident\/7\./))
+      return 11;
+
+    else
+      return 0; //It is not IE
+  }
   handleResize = () => {
     this.setState({
       mediaWidth: window.innerWidth > 0 ? window.innerWidth : screen.width
@@ -18,18 +33,22 @@ export default class BackgroundScrollFade extends React.Component {
   }
   render = () => {
     let backgroundStyle = () => {
-      if(this.state.mediaWidth > 868) {
-        return { backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0) ${this.props.percentage/4}%, rgba(255,255,255,1) ${this.props.percentage}%)`}
-      } else {
+      if(GetIEVersion > 0) {
         return { backgroundImage: `linear-gradient(to top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`}
+      } else {
+        if(this.state.mediaWidth > 868) {
+          return { backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0) ${this.props.percentage/4}%, rgba(255,255,255,1) ${this.props.percentage}%)`}
+        } else {
+          return { backgroundImage: `linear-gradient(to top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)`}
+        }  
       }
     }
     return (
       <div
         id={this.props.id}
+        style={backgroundStyle()}
         className="backgroundScrollFade"
         // style={{height: + 100-this.props.percentage + "%"}}
-        style={backgroundStyle()}
         >
         </div>
     )
