3e88ebea06b56d648c6e6ef77d3b013a2a31ac30
thomasheimstad <thomasheimstad@gmail.com>
2018-02-26 21:50:54 +0100

    Navlink titleIncrease & Decrease

--- src/components/modules/Controls.jsx
+++ src/components/modules/Controls.jsx
@@ -3,18 +3,20 @@ import NavLink from 'gatsby-link';
 
 export default class Controls extends React.Component {
   render = () => {
+    let increaseOpus = parseInt(this.props.post.title) + 1;
+    let decreaseOpus = parseInt(this.props.post.title) - 1;
     return (
       <div className="controls flex spaceBetween">
         <div>
           { this.props.post.title == "1" ?
             <NavLink exact to="/"><h3>&larr;</h3></NavLink> :
-            <NavLink exact to={`opus${parseInt(this.props.post.title)-1}`}><h3>&larr;Op. {parseInt(this.props.post.title)-1}</h3></NavLink>
+            <NavLink exact to={`opus${decreaseOpus}`}><h3>&larr;Op. {decreaseOpus}</h3></NavLink>
           }
         </div>
         <div>
           { this.props.post.title == "74" ?
             <NavLink exact to="/"><h3>&rarr;</h3></NavLink> :
-            <NavLink exact to={`opus${parseInt(this.props.post.title)+1}`}><h3>Op. {parseInt(this.props.post.title)+1}&rarr;</h3></NavLink>
+            <NavLink exact to={`opus${increaseOpus}`}><h3>Op. {increaseOpus}&rarr;</h3></NavLink>
           }
         </div>
       </div>
