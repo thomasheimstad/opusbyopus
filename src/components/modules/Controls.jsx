31f84afd708027455fe53b5e589d238e84c03e80
thomasheimstad <thomasheimstad@gmail.com>
2018-02-27 15:44:44 +0100

    NavLink with prefix
    Url-test

--- src/components/modules/Controls.jsx
+++ src/components/modules/Controls.jsx
@@ -9,13 +9,13 @@ export default class Controls extends React.Component {
         <div>
           { this.props.post.title == "1" ?
             <NavLink exact to="/"><h3>&larr;</h3></NavLink> :
-            <NavLink exact to={`opus${parseOpusInt-1}`}><h3>&larr;Op. {parseOpusInt-1}</h3></NavLink>
+            <NavLink exact to={`/opus${parseOpusInt-1}`}><h3>&larr;Op. {parseOpusInt-1}</h3></NavLink>
           }
         </div>
         <div>
           { this.props.post.title == "74" ?
             <NavLink exact to="/"><h3>&rarr;</h3></NavLink> :
-            <NavLink exact to={`opus${parseOpusInt+1}`}><h3>Op. {parseOpusInt+1}&rarr;</h3></NavLink>
+            <NavLink exact to={`/opus${parseOpusInt+1}`}><h3>Op. {parseOpusInt+1}&rarr;</h3></NavLink>
           }
         </div>
       </div>
