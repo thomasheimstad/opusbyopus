c270c99a9127e6528a67f6d949170cdac5653703
thomasheimstad <thomasheimstad@gmail.com>
2018-03-16 17:37:17 +0100

    Bug: Black hero-background. Test add height to overcast

--- src/components/modules/Hero.jsx
+++ src/components/modules/Hero.jsx
@@ -9,7 +9,7 @@ export default class Hero extends React.Component {
       <div className="hero flex center">
         {/*<div id={this.props.id} style={styles}>*/}
         <BgImage sizes={this.props.src}  height={this.props.height} position={this.props.position} fit={this.props.fit} />
-        <Overcast id={this.props.id} />
+        <Overcast id={this.props.id} height="100"/>
       </div>
     )
   }
