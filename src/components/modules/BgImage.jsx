72c17e92b32dc1452e2b970bd240daf3cbe00c37
thomasheimstad <thomasheimstad@gmail.com>
2018-03-21 09:59:30 +0100

    Bug: missing artistCard image. z-index test

--- src/components/modules/BgImage.jsx
+++ src/components/modules/BgImage.jsx
@@ -7,7 +7,7 @@ const StyledImage = styled(Image)`
   top: 0;
   left: 0;
   width: 100%;
-  z-index: -1;
+  z-index: 1;
   height: ${props => props.height || 'auto'};
 
   // Adjust image positioning (if image covers area with defined height) and add font-family for polyfill
