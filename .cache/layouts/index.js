40d31121f312da96c7ac96cd3d0df0d655126178
thomasheimstad <thomasheimstad@gmail.com>
2018-06-06 01:10:52 +0200

    Testing public folder upload

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/thomasheimstad/Sites/Obo/src/layouts/index.jsx"
-  import data from "/Users/thomasheimstad/Sites/Obo/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
