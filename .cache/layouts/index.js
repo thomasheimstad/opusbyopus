8bee07768e0cce3bc5fed14c57bb76cf3b2919b7
thomasheimstad <thomasheimstad@gmail.com>
2018-06-06 00:22:36 +0200

    Test upload cache

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/thomasheimstad/Sites/Obo/src/layouts/index.jsx"
+  import data from "/Users/thomasheimstad/Sites/Obo/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
